9017dba0c3b5fa3e4f78ff039905a91e9ce5654a
ahsanaaxioned <ahsana@axioned.com>
2023-07-12 00:05:16 +0530

    go back and go forward added with state

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,16 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-const About = ()=>{ return <h1>Hey user.! this our about us  page</h1> }
+const About = ()=>{ 
+    const navigate = useNavigate();
+    const location  = useLocation();
+    return (
+        <>
+          <div className="home">
+          <h1>Hey user.! this our about us  page and my id is {location.state.id}</h1>
+          <button className="btn" onClick={()=>navigate((-1))}>Go Back</button>
+          </div>
+        </>
+      );
+ }
 export default About
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Home = ()=> { return <h1>Hey user.! this our Home page</h1> }
-export default Home
+const Home = () => {
+    const navigate = useNavigate();
+    const id = 10;
+  return (
+    <>
+      <div className="home">
+      <h1>Hey user.! this our Home page</h1>
+      <button className="btn" onClick={()=>navigate( (1), {state: {id:id}})}>Go Forward</button>
+      </div>
+    </>
+  );
+};
+export default Home;
